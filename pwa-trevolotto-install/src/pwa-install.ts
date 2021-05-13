d486d275b65a893e6a2561ec6899ac06d270c3b6
Ronda01 <37518388+ronda01@users.noreply.github.com>
2021-05-13 12:31:22 -0300

    Update pwa-install.ts

--- pwa-trevolotto-install/src/pwa-install.ts
+++ pwa-trevolotto-install/src/pwa-install.ts
@@ -31,13 +31,13 @@ export class pwainstall extends LitElement implements PWAInstallComponent {
   @property({ type: Array }) relatedApps: any[] = [];
 
   @property({ type: String }) explainer: string =
-    "This app can be installed on your PC or mobile device.  This will allow this web app to look and behave like any other installed app.  You will find it in your app lists and be able to pin it to your home screen, start menus or task bars.  This installed web app will also be able to safely interact with other apps and your operating system. ";
+    "Este aplicativo pode ser instalado em seu PC ou dispositivo móvel. Isso permitirá que este aplicativo da web se pareça e se comporte como qualquer outro aplicativo instalado. Você o encontrará em suas listas de aplicativos e poderá fixá-lo em sua tela inicial, menus de início ou barras de tarefas. Este aplicativo da web instalado também será capaz de interagir com segurança com outros aplicativos e seu sistema operacional.";
   @property({ type: String }) featuresheader: string = "Key Features";
-  @property({ type: String }) descriptionheader: string = "Description";
-  @property({ type: String }) installbuttontext: string = "Install";
-  @property({ type: String }) cancelbuttontext: string = "Cancel";
+  @property({ type: String }) descriptionheader: string = "Descrição";
+  @property({ type: String }) installbuttontext: string = "Instalar";
+  @property({ type: String }) cancelbuttontext: string = "Cancelar";
   @property({ type: String }) iosinstallinfotext: string =
-    "Tap the share button and then 'Add to Homescreen'";
+    "Toque no botão compartilhar e selecione 'Tela de Início'";
 
   @property() deferredprompt: any;
 
